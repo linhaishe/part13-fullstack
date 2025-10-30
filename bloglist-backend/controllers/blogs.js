a21d351e9144cceec4932c00a77cc7ae6f3da23c
miamiamia <42030043+linhaishe@users.noreply.github.com>
2025-10-30 20:19:56 +0800

    feat: Exercise 13.14.

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -16,10 +16,24 @@ const blogFinder = async (req, res, next) => {
 
 router.get("/", async (req, res) => {
   try {
-    const where = {};
+    let where = {};
     if (req.query.search) {
-      where.title = {
-       [Op.iLike]: `%${req.query.search}%`
+      // where.title = {
+      //  [Op.iLike]: `%${req.query.search}%`
+      // };
+      where = {
+        [Op.or]: [
+          {
+            title: {
+              [Op.iLike]: `%${req.query.search}%`,
+            },
+          },
+          {
+            author: {
+              [Op.iLike]: `%${req.query.search}%`,
+            },
+          },
+        ],
       };
     }
     const blogs = await Blog.findAll({
