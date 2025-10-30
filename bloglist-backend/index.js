5bebee7c2368f59a213befd7a9ef1623381591e3
miamiamia <42030043+linhaishe@users.noreply.github.com>
2025-10-30 17:46:52 +0800

    feat: Exercise 13.7.

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/blogs", blogsRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(500).json({ error: err.message })
+})
+
 const start = async () => {
   await connectDb();
   app.listen(config.PORT, () => {
