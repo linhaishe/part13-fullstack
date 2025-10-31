ac88683bc9be91553c185e67f49bc03d3bebaf22
miamiamia <42030043+linhaishe@users.noreply.github.com>
2025-10-31 16:45:30 +0800

    feat: Exercise 13.22.

--- bloglist-backend/controllers/userMarks.js
+++ bloglist-backend/controllers/userMarks.js
@@ -7,4 +7,19 @@ router.get("/", async (req, res) => {
   res.json(readingList);
 });
 
+router.put("/:id", async (req, res) => {
+  const decodedToken = req.decodedToken;
+  const markedBlog = await UserMarks.findByPk(req.params.id);
+
+  if (markedBlog.userId.toString() !== decodedToken.id.toString()) {
+    return res
+      .status(403)
+      .json({ error: "only the creator can mark read this blog" });
+  }
+
+  markedBlog.isRead = req.body.read;
+  await markedBlog.save();
+  res.json(markedBlog);
+});
+
 export default router;
